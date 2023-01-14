0b4429fc29b9e0f9f09ccf7609814495f416a4ae
Sander van der Vliet <sander@digitalnatives.nl>
2023-01-15 00:45:30 +0100

    Create motd.sh

---
+++ motd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+normal=$(tput sgr0)
+green=$(tput setaf 2)
+blue=$(tput setaf 4)
+yellow=$(tput setaf 3)
+
+cat << EOF
+
+${green}Welcome!${normal}
+
+${blue}System Information: ${normal}
+${blue}----------------------------------------------------------------${normal}
+${blue}The current working directory is:  ${yellow}$PWD${normal}
+${blue}You are logged in as:              ${yellow}$(whoami)${normal}
+${blue}Uptime:                            ${yellow}$(uptime)${normal}
+${blue}PHP version:                       ${yellow}$(php -v)${normal}
+${blue}----------------------------------------------------------------${normal}
+
+${blue}Running Processes: ${normal}
+${blue}----------------------------------------------------------------${normal}
+${yellow}$(ps axuf)${normal}
+${blue}----------------------------------------------------------------${normal}
+
+${blue}Your sudo rights: ${normal}
+${blue}----------------------------------------------------------------${normal}
+${yellow}$(sudo -l)${normal}
+${blue}----------------------------------------------------------------${normal}
+EOF
